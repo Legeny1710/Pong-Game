f197e1bd72be42420eb9cc13441867b20d444ae6
Yusuf Kula <yvk2045@gmail.com>
2023-08-02 21:47:45 +0100

    Program now can detect if ball went out of boundaries, according to which way it went out, it adds a point to according player, it increases the speed as game goes on. Game stops when either of the player has 5 point. When game stops it displays that 'game is over' and displays which side win the game

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from turtle import Screen
+from turtle import Screen,Turtle
 from paddle import Paddle
 from ball import Ball
+from score_board import  ScoreBoard
 import time
 
 screen = Screen()
@@ -10,12 +11,18 @@ screen.title("Pong Game")
 
 screen.tracer(0)
 
+game_over = Turtle()
+game_over.color("white")
+game_over.penup()
+game_over.hideturtle()
+
 
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
 
 
 pong_ball = Ball()
+scoreBoard = ScoreBoard()
 
 screen.listen()
 screen.onkey(key="Up", fun=r_paddle.move_paddle_up)
@@ -28,7 +35,7 @@ screen.onkey(key="s", fun=l_paddle.move_paddle_down)
 
 is_game_on = True
 while is_game_on:
-    time.sleep(0.05)
+    time.sleep(pong_ball.move_speed)
     screen.update()
     pong_ball.move()
 
@@ -40,6 +47,27 @@ while is_game_on:
 
     if pong_ball.distance(l_paddle) < 60 and pong_ball.xcor() < -340:
         pong_ball.bounce_x()
+
+    if pong_ball.xcor() > 380:
+        pong_ball.reset_position()
+        scoreBoard.l_point()
+
+    if pong_ball.xcor() < -380:
+        pong_ball.reset_position()
+        scoreBoard.r_point()
+
+    if scoreBoard.r_score == 5 or scoreBoard.l_score == 5:
+        is_game_on = False
+        game_over.goto(0, 0)
+        game_over.write("Game is Over!", align="center", font=("Courier", 60, "normal"))
+        if scoreBoard.r_score == 5:
+            game_over.goto(0, 100)
+            game_over.write("Player on the RIGHT wins!", align="center", font=("Courier", 30, "normal"))
+        elif scoreBoard.l_score == 5:
+            game_over.goto(0, 100)
+            game_over.write("Player on the LEFT wins!", align="center", font=("Courier", 30, "normal"))
+
+
 
 
 
